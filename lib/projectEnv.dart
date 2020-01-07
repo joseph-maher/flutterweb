34fe00976b7247e410805286532bd4b4d00a1e97
Joseph Maher <35913191+seosamhomeachair@users.noreply.github.com>
2020-01-07 17:37:11 +1100

    Attempting to locate browser

---
+++ lib/projectEnv.dart
@@ -0,0 +1,8 @@
+
+class Project{
+  static var url = "https://internmatch-test.gada.io";
+  static var projectColor = 0xFF233A4E;
+  static var projectName ="InternMatch";
+  var fontsize =3;
+  static var img = "images/internmatch_logo.png";
+}
